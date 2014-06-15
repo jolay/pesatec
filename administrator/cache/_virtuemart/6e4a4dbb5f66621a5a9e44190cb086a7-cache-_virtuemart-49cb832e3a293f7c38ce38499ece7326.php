ad0bacc6be1a5a8be3ecd9a530048e8ead28f5f4
Jorge Olay <jolay@intelisis.com>
2014-06-14 21:53:56 -0500

    Upload 1
    Upload 1

---
+++ administrator/cache/_virtuemart/6e4a4dbb5f66621a5a9e44190cb086a7-cache-_virtuemart-49cb832e3a293f7c38ce38499ece7326.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";s:196:"<option   value="2">Air Conditioner</option><option   value="1">Coffee Machines</option><option selected=\"selected\"  value="3">Refrigerators</option><option   value="4">Washing Machines</option>";}
---
+++ administrator/cache/_virtuemart/6e4a4dbb5f66621a5a9e44190cb086a7-cache-_virtuemart-b0f7a096960abeb293d2eb511315e1c5.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";s:175:"<option   value="2">Air Conditioner</option><option   value="1">Coffee Machines</option><option   value="3">Refrigerators</option><option   value="4">Washing Machines</option>";}
